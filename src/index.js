865acfc97ab8f0562bff925a431dd45a8d0c10e0
sheila <sheila18.sa@gmail.com>
2021-10-15 13:06:00 +0200

    change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // create and config server
 const server = express();
@@ -7,30 +7,42 @@ server.use(cors());
 server.use(express.json());
 
 //static server
-const serverStaticPath = './public';
+const serverStaticPath = "./public";
 server.use(express.static(serverStaticPath));
 
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-    console.log(`Server listening at http://localhost:${serverPort}`);
+  console.log(`Server listening at http://localhost:${serverPort}`);
 });
 //construir un objeto con los valores data.cardURL data.error data.success
 
 server.post("/card", (req, res) => {
+  const card = {
+    cardURL: "",
+    error: "",
+    success: true,
+  };
+  if (
+    req.body.palette !== "" &&
+    req.body.photo !== "" &&
+    req.body.name !== "" &&
+    req.body.job !== "" &&
+    req.body.phone !== "" &&
+    req.body.email !== "" &&
+    req.body.linkedin !== "" &&
+    req.body.github !== ""
+  ) {
     const card = {
-        cardURL: "",
-        error: "",
-        success: true
-    }
-    if(req.body.palette !== '' && req.body.photo !== '' && req.body.name !== ''&& req.body.job !== '' && req.body.phone !== '' && req.body.email !== ''&& req.body.linkedin !== '' && req.body.github !== ''){
-        const card = {
-            cardURL: "localhost:4000",
-            success: true
-        }} else {const card = {
-            error: "rellena los campos",
-            success: false
-    }}
-    console.log(card)
-    res.json(card)
-})
+      cardURL: "localhost:4000",
+      success: true,
+    };
+  } else {
+    const card = {
+      error: "rellena los campos",
+      success: false,
+    };
+  }
+  console.log(card);
+  res.json(card);
+});
